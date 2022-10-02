e6598cab7f9129fe9793b1b1005d0f34c86255f6
Triyono <yono.tri@gmail.com>
2022-10-02 15:46:44 +0700

    fix splash

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'package:catatan_harian/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:catatan_harian/screen/login_screen.dart';
+import 'package:hive/hive.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,12 +13,22 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 3)).then((value) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (_) => LoginScreen(),
-        ),
-      );
+    Future.delayed(Duration(seconds: 3)).then((value) async {
+      var box = Hive.box('userBox');
+      var uid = box.get('uid');
+      if (uid == null) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (_) => LoginScreen(),
+          ),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (_) => HomeScreen(),
+          ),
+        );
+      }
     });
 
     super.initState();
